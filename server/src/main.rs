0818125d51bedb42f0044e26ccc923d777745854
dusk <y.bera003.06@protonmail.com>
2025-08-08 19:06:34 +0300

    Revert "feat(server): convert microsecond timestamps to seconds"
    This reverts commit 5cc9c68cdef616b5227eea2d96ba01048edf2fab.

--- server/src/main.rs
+++ server/src/main.rs
@@ -322,14 +322,9 @@ fn migrate() {
                 to.ingest_events(hits.map(|hit| {
                     count += 1;
                     let hit = hit.expect("cant decode hit");
-                    let mut timestamp = hit.timestamp;
-                    // check if timestamp is microseconds, convert to seconds if it is
-                    if timestamp > 10_000_000_000 {
-                        timestamp /= 1_000_000;
-                    }
                     EventRecord {
                         nsid: nsid.to_smolstr(),
-                        timestamp,
+                        timestamp: hit.timestamp,
                         deleted: hit.deser().unwrap().deleted,
                     }
                 }))
